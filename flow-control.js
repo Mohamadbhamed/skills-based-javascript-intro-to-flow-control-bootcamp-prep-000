c083b0173bbbcd3695a7a470878d40134039781b
Mohamad Hamed <mohamad.b.hamed@hotmail.com>
2017-04-11 17:26:41 +0000

    Old code that is not working.

--- flow-control.js
+++ flow-control.js
@@ -1,9 +1,17 @@
-function basicTeenager(age) {
+const expect = require('expect') const fs = require('fs') const jsdom = require('jsdom') const path = require('path'
 
+
+function basicTeenager(age) {
+  if(age >= 13 && age <=19) {
+    return "You are a teenager!"}
 }
+basicTeenager(14)
 
 function teenager(age) {
-
+if (age >= 13 && age <=19) {
+  return You are a teenager! }
+} else {
+  return You are not a teenager!
 }
 
 function ageChecker(age) {
